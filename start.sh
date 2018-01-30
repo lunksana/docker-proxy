6dfa5871936b48120603ed34828f18b4c59ba60d
lunksana <zoufeng4@gmail.com>
2018-01-30 09:43:16 +0800

    fix bug 4

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ if [ ${SS_MOD} == "ss-server" ]; then
     fi
 else
     if [ ${ENABLE_OBFS} != 'true' ]; then
-        ${SS_MOD} -s ${SERVER_HOST} -p ${SERVER_PORT} -k ${PASSWORD} -m ${METHOD} -l 1080 -u
+        ${SS_MOD} -s ${SERVER_HOST} -p ${SERVER_PORT} -k ${PASSWORD} -m ${METHOD} -b 0.0.0.0 -l 1080 -u
     else
-        ${SS_MOD} -s ${SERVER_HOST} -p ${SERVER_PORT} -k ${PASSWORD} -m ${METHOD} -l 1080 -u --plugin obfs-local --plugin-opts ${PLUGIN_OPTS_LOCAL}
+        ${SS_MOD} -s ${SERVER_HOST} -p ${SERVER_PORT} -k ${PASSWORD} -m ${METHOD} -b 0.0.0.0 -l 1080 -u --plugin obfs-local --plugin-opts ${PLUGIN_OPTS_LOCAL}
     fi
 fi
